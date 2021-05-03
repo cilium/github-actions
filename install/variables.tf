cilium/github-actions

commit b5d6bff47d5e609c1d216302b456e9409ddee36d

client: unassign PR reviewer on approval
Users marked for requested reviews get assignments for the PR. Once they
submit a review, and this review is an approval of the pull request,
they are supposed to unassign themselves from the PR. This helps
Janitors finding PRs that need their attention. But removing the
assignment is currently done manually, and users often omit it.

Let's automate unassignment on review, when the review concludes with an
approval of the PR.

Signed-off-by: Quentin Monnet <quentin@isovalent.com>

pkg/client.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"strings"
 	"time"
 
 	gh "github.com/google/go-github/v32/github"
@@ -186,6 +187,23 @@ func (c *Client) HandlePRRE(cfg PRBlockerConfig, pre *gh.PullRequestReviewEvent)
 	}).Msg("Action triggered from PR")
 
 	c.prLabels = ParseGHLabels(pr.Labels)
+
+	// Unassign reviewer on approval
+	if pre.Review != nil {
+		if strings.ToLower(pre.Review.GetState()) == "approve" {
+			users := []*gh.User{
+				pre.Review.GetUser(),
+			}
+			err := c.Unassign(context.TODO(), owner, repoName, prNumber, users)
+			if err != nil {
+				c.log.Info().Fields(map[string]interface{}{
+					"error":     err,
+					"pr-number": prNumber,
+				}).Msg("Unable to remove assignees from PR")
+				return err
+			}
+		}
+	}
 
 	// if len(cfg.AutoMerge.Label) != 0 {
 	if true {

pkg/assignees.go
@@ -20,14 +20,16 @@ import (
 	gh "github.com/google/go-github/v32/github"
 )
 
-func (c *Client) Assign(ctx context.Context, owner string, name string, number int, reviewers []*gh.User) (err error) {
+type assignHandler func(ctx context.Context, owner, repo string, number int, assignees []string) (*gh.Issue, *gh.Response, error)
+
+func (c *Client) manageAssignees(ctx context.Context, owner string, name string, number int, reviewers []*gh.User, handler assignHandler, msg string) (err error) {
 	assignees := make([]string, 0, len(reviewers))
 	defer func() {
 		c.log.Info().Fields(map[string]interface{}{
 			"error":     err,
 			"assignees": assignees,
 			"pr-number": number,
-		}).Msg("Added assignees to PR")
+		}).Msg(msg)
 	}()
 	for _, user := range reviewers {
 		assignees = append(assignees, user.GetLogin())
@@ -35,7 +37,7 @@ func (c *Client) Assign(ctx context.Context, owner string, name string, number i
 	if len(assignees) == 0 {
 		return nil
 	}
-	_, _, err = c.gh.Issues.AddAssignees(
+	_, _, err = handler(
 		ctx,
 		owner,
 		name,
@@ -47,4 +49,14 @@ func (c *Client) Assign(ctx context.Context, owner string, name string, number i
 	}
 
 	return nil
+}
+
+func (c *Client) Assign(ctx context.Context, owner string, name string, number int, users []*gh.User) (err error) {
+	return c.manageAssignees(ctx, owner, name, number, users,
+		c.gh.Issues.AddAssignees, "Added assignees to PR")
+}
+
+func (c *Client) Unassign(ctx context.Context, owner string, name string, number int, users []*gh.User) (err error) {
+	return c.manageAssignees(ctx, owner, name, number, users,
+		c.gh.Issues.RemoveAssignees, "Remove assignees from PR")
 }

commit a147f1a26af399e2a6a7f34995eacf0ac8798e68

client: fix unassignment on 'approved' PRs
Fixes: b5d6bff47d5e ("client: unassign PR reviewer on approval")
Signed-off-by: André Martins <andre@cilium.io>

install/variables.tf
@@ -38,5 +38,5 @@ variable "service_account_name" {
 variable "container_image" {
   description = "The image name to deploy"
   type        = string
-  default     = "quay.io/cilium/github-actions:v1.0.1"
+  default     = "quay.io/cilium/github-actions:v1.0.2"
 }

pkg/client.go
@@ -190,7 +190,7 @@ func (c *Client) HandlePRRE(cfg PRBlockerConfig, pre *gh.PullRequestReviewEvent)
 
 	// Unassign reviewer on approval
 	if pre.Review != nil {
-		if strings.ToLower(pre.Review.GetState()) == "approve" {
+		if strings.ToLower(pre.Review.GetState()) == "approved" {
 			users := []*gh.User{
 				pre.Review.GetUser(),
 			}